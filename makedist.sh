55aa886e38ad67ed25a2cf1b2a1e0b4118b70a2b
Jason Siefken <siefkenj@gmail.com>
2024-08-30 12:58:45 -0400

    Recompile

--- makedist.sh
+++ makedist.sh
@@ -2,10 +2,10 @@
 
 cd book
 # Make version of the book
+latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes &
 latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-book &
-#latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" linearalgebra-book &
-#latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" linearalgebra-solutions &
-#latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" linearalgebra-instructor &
+#latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-solutions &
+#latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-instructor &
 latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-slides &
 
 # We process all the latex files in parallel and wait for them to finish
