71eeadf3442cf6186d708420d22e36051d1b594c
Jason Siefken <siefkenj@gmail.com>
2024-09-12 14:57:23 -0400

    Add version of slides with white border

--- makedist.sh
+++ makedist.sh
@@ -7,6 +7,7 @@ latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-li
 #latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-solutions &
 #latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-instructor &
 latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-slides &
+latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" odes-white &
 
 # We process all the latex files in parallel and wait for them to finish
 wait $(jobs -p)
